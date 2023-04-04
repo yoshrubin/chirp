348b7196acc3ba104ad4297c2684c71b30ab5976
Yoshi Rubin <yoshrubin@gmail.com>
2023-04-04 16:50:15 +0300

    added new profile and post routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -86,8 +87,13 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col gap-1">
         <div className="flex gap-1 text-slate-300">
-          <span>{`@${author.username}`}</span> ·
-          <span>{dayjs(post.createdAt).fromNow()}</span>
+          <Link href={`@${author.username}`}>
+            <span>{`@${author.username}`}</span>
+          </Link>
+          ·
+          <Link href={`/post/${post.id}`}>
+            <span>{dayjs(post.createdAt).fromNow()}</span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>

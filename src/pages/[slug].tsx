45ccabd069f684164a302c6a12b4db8348b7424e
Yoshi Rubin <yoshrubin@gmail.com>
2023-04-14 11:22:08 +0300

    design profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -13,7 +13,20 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <title>{data.username}s Profile</title>
       </Head>
       <PageLayout>
-        <div>{data.username}</div>
+        <div className="relative h-36  bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${data.username ?? ""}'s profile image`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-16 ml-4 rounded-full border-4 border-black bg-black"
+          />
+        </div>
+        <div className="h-16"></div>
+        <div className="p-8 text-2xl font-bold">{`@${
+          data.username ?? ""
+        }`}</div>
+        <div className="w-full border-b border-slate-400"></div>
       </PageLayout>
     </>
   );
@@ -24,6 +37,7 @@ import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
 import superjson from "superjson";
 import { PageLayout } from "~/components/layout";
+import Image from "next/image";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const ssg = createProxySSGHelpers({

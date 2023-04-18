8701aa7ad34dcf38cb4b7d606a5f7ff8626d6a01
Yoshi Rubin <yoshrubin@gmail.com>
2023-04-18 20:33:33 +0300

    render user posts on profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,6 +1,27 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { LoadingPage } from "~/components/loading";
 import { api } from "~/utils/api";
+import { PageLayout } from "~/components/layout";
+import Image from "next/image";
+import { PostView } from "~/components/postview";
+
+const ProfileFeed = (props: { userId: string }) => {
+  const { data, isLoading } = api.posts.getPostsByUserId.useQuery({
+    userId: props.userId,
+  });
+
+  if (isLoading) return <LoadingPage />;
+  if (!data || data.length === 0) return <div>No Posts</div>;
+
+  return (
+    <div className="flex flex-col">
+      {data.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
+  );
+};
 
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data } = api.profile.getUserByUsername.useQuery({
@@ -27,6 +48,7 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
           data.username ?? ""
         }`}</div>
         <div className="w-full border-b border-slate-400"></div>
+        <ProfileFeed userId={data.id} />
       </PageLayout>
     </>
   );
@@ -36,8 +58,6 @@ import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
 import superjson from "superjson";
-import { PageLayout } from "~/components/layout";
-import Image from "next/image";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const ssg = createProxySSGHelpers({
